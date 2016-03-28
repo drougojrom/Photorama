b899338c0c016e2b225b20f896185329e85a4031
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-03-28 13:35:20 +0300

    added API for flickr

---
+++ Photorama/FlickrAPI.swift
@@ -0,0 +1,49 @@
+//
+//  FlickrAPI.swift
+//  Photorama
+//
+//  Created by Roman Ustiantcev on 28/03/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import Foundation
+
+enum Method: String {
+    case RecentPhotos = "flickr.photos"
+}
+
+struct FlickrAPI {
+    
+    private static let baseURLString = "https://api.flickr.com/services/rest"
+    private static let APIKey = "073a626094e442c586d1642e7ed079c5"
+    
+    private static func flickrURL(method method: Method, parameters: [String:String]?) -> NSURL {
+        
+        let components = NSURLComponents(string: baseURLString)!
+        var queryItems = [NSURLQueryItem]()
+        
+        let baseParams = ["method": method.rawValue,
+                          "format": "json",
+                          "nojsoncallback": 1,
+                          "api_key": APIKey
+                         ]
+        for (key, value) in baseParams {
+            let item = NSURLQueryItem(name: key as! String, value: (value as! String))
+            queryItems.append(item)
+        }
+        
+        if let additionalParams = parameters {
+            for (key, value) in additionalParams {
+                let item = NSURLQueryItem(name: key, value: value)
+                queryItems.append(item)
+            }
+        }
+        components.queryItems = queryItems
+        return components.URL!
+    }
+    
+    static func recentPhotosURL() -> NSURL {
+        return flickrURL(method : .RecentPhotos, parameters: ["extras": "url_h, date_taken"])
+    }
+    
+}
---
+++ Photorama/PhotoStore.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Roman Ustiantcev on 28/03/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import Foundation
+
+class PhotoStore {
+    
+    
+    
+    
+}
