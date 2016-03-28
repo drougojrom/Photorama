d87547c398dc8ad99256aa39ed668e32dd690d5d
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-03-28 11:58:54 +0300

    added initial files

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Roman Ustiantcev on 28/03/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
