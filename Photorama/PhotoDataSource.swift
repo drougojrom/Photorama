612095bae0a0ade83754d477042b86bd1e5bd491
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-03-29 16:24:47 +0300

    added new class and collection view

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Roman Ustiantcev on 29/03/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    
+    
+}
