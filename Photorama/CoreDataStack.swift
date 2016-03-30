9503737dc95ab25dd4663c1cd4dcdbde9b276630
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-03-30 20:23:02 +0300

    added persistentStoreCoordinator

--- Photorama/CoreDataStack.swift
+++ Photorama/CoreDataStack.swift
@@ -13,6 +13,24 @@ class CoreDataStack {
     
     let managedObjectModelName: String
     
+    private var applicationDocumentDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls.first!
+    }()
+    
+    
+    // MARK : setup persistent store coordinator (where saving/loading/enteties?)
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        
+        var coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let pathComponent = "\(self.managedObjectModel).sqlite"
+        let url = self.applicationDocumentDirectory.URLByAppendingPathComponent(pathComponent)
+        
+        let store = try! coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        return coordinator
+    }()
+    
     private lazy var managedObjectModel: NSManagedObjectModel = {
         
         let modelURL = NSBundle.mainBundle().URLForResource(self.managedObjectModelName, withExtension: "momd")!
