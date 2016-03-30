a0e1914086c23e7dcb4a552de6180e7facf8abba
Roman Ustuantsev <liberrom.hse@gmail.com>
2016-03-30 12:18:30 +0300

    added new class for image cell

---
+++ Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Roman Ustiantcev on 30/03/16.
+//  Copyright © 2016 Roman Ustiantcev. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    func updateWithImage(image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
